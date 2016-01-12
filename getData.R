b6916be810fb432f962ef736b4bc9c7b465c2beb
markdunning <mark.dunning@gmail.com>
2016-01-12 14:48:53 +0000

    add more packages
    pre-download GEO data

--- getData.R
+++ getData.R
@@ -9,3 +9,10 @@ write.table(fData(nki), file="nki/NKI.fData.txt",sep="\t",quote=FALSE)
 datadir <- system.file("extdata", package="estrogen")
 dir.create("estrogen")
 sapply(list.files(datadir), function(x) file.copy(paste(datadir,x,sep="/"),"estrogen"))
+
+remotefile <- 'ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE3nnn/GSE3156/matrix/GSE3156_series_matrix.txt.gz'
+download.file(remotefile, destfile="data/GSE3156_series_matrix.txt.gz")
+
+url <- "ftp://ftp.ncbi.nih.gov/pub/geo/DATA/SeriesMatrix/GSE33126/"
+filenm <- "GSE33126_series_matrix.txt.gz"
+if(!file.exists("GSE33126_series_matrix.txt.gz")) download.file(paste(url, filenm, sep=""), destfile=paste0("data/",datafilenm))
--- installBiocPkgs.R
+++ installBiocPkgs.R
@@ -3,4 +3,4 @@ options(BioC_mirror = c("Cambridge" = "http://mirrors.ebi.ac.uk/bioconductor/"))
 options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
 biocLite(c("limma","affy","affyPLM","beadarray","GEOquery","genefilter","illuminaHumanv3.db","cluster",
            "ggplot2","GOstats","breastCancerVDX","breastCancerTRANSBIG","pamr","survival",
-           "estrogen"))
+           "estrogen","ArrayExpress","RColoeBrewer","cgdsr"))
