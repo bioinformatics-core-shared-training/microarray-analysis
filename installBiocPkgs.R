bbe93bc9fbe015b00c781d14669ab6cb5911c52c
markdunning <mark.dunning@gmail.com>
2016-01-12 15:07:14 +0000

    correct typo in package name

--- installBiocPkgs.R
+++ installBiocPkgs.R
@@ -2,5 +2,5 @@ source("http://www.bioconductor.org/biocLite.R")
 options(BioC_mirror = c("Cambridge" = "http://mirrors.ebi.ac.uk/bioconductor/"))
 options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
 biocLite(c("limma","affy","affyPLM","beadarray","GEOquery","genefilter","illuminaHumanv3.db","cluster",
-           "ggplot2","GOstats","breastCancerVDX","breastCancerTRANSBIG","pamr","survival",
-           "estrogen","ArrayExpress","RColoeBrewer","cgdsr"))
+           "ggplot2","GOstats","breastCancerVDX","breastCancerTRANSBIG","breastCancerNKI","pamr","survival",
+           "estrogen","ArrayExpress","RColorBrewer","cgdsr"))
