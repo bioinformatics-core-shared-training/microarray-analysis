efb56b524a82de3e05629c4a72c6541c977a23e4
markdunning <mark.dunning@gmail.com>
2016-01-12 15:41:36 +0000

    new data directory

--- getData.R
+++ getData.R
@@ -1,13 +1,13 @@
 library(Biobase)
 library(breastCancerNKI)
 data(nki)
-dir.create("nki")
+dir.create("data/nki")
 write.table(exprs(nki), file="nki/NKI.exprs.txt",sep="\t",quote=FALSE)
 write.table(pData(nki), file="nki/NKI.pData.txt",sep="\t",quote=FALSE)
 write.table(fData(nki), file="nki/NKI.fData.txt",sep="\t",quote=FALSE)
 
 datadir <- system.file("extdata", package="estrogen")
-dir.create("estrogen")
+dir.create("data/estrogen")
 sapply(list.files(datadir), function(x) file.copy(paste(datadir,x,sep="/"),"estrogen"))
 
 remotefile <- 'ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE3nnn/GSE3156/matrix/GSE3156_series_matrix.txt.gz'
