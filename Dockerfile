24fd3df0dbbb5e2f78a37c4593a6dd8f8d214d08
markdunning <mark.dunning@gmail.com>
2016-01-13 13:38:04 +0000

    add new annotation package to list
    make sure estrogen directory is present

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ MAINTAINER Mark Dunning<mark.dunning@cruk.cam.ac.uk>
 ###Get repository of the course. Install data and R packages
 RUN git clone https://github.com/bioinformatics-core-shared-training/microarray-analysis.git /home/rstudio/
 WORKDIR /home/rstudio
+RUN mkdir estrogen
 RUN mkdir data/
 RUN R -f installBiocPkgs.R
 RUN R -f getData.R
--- installBiocPkgs.R
+++ installBiocPkgs.R
@@ -3,4 +3,4 @@ options(BioC_mirror = c("Cambridge" = "http://mirrors.ebi.ac.uk/bioconductor/"))
 options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
 biocLite(c("limma","affy","affyPLM","beadarray","GEOquery","genefilter","illuminaHumanv3.db","cluster",
            "ggplot2","GOstats","breastCancerVDX","breastCancerTRANSBIG","breastCancerNKI","pamr","survival",
-           "estrogen","ArrayExpress","RColorBrewer","cgdsr"))
+           "estrogen","ArrayExpress","RColorBrewer","cgdsr","hgu95av2cdf"))
