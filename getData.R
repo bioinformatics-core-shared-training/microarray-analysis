50878838bdb785691f7ec3982e6c79fa1796b1b3
markdunning <mark.dunning@gmail.com>
2016-02-22 14:00:13 +0000

    update getting data R script

--- getData.R
+++ getData.R
@@ -1,18 +1,19 @@
 library(Biobase)
 library(breastCancerNKI)
-data(nki)
-dir.create("data/nki")
-write.table(exprs(nki), file="data/nki/NKI.exprs.txt",sep="\t",quote=FALSE)
-write.table(pData(nki), file="data/nki/NKI.pData.txt",sep="\t",quote=FALSE)
-write.table(fData(nki), file="data/nki/NKI.fData.txt",sep="\t",quote=FALSE)
 
 datadir <- system.file("extdata", package="estrogen")
-dir.create("data/estrogen")
+dir.create("data/estrogen",showWarnings = FALSE)
 sapply(list.files(datadir), function(x) file.copy(paste(datadir,x,sep="/"),"estrogen"))
 
-remotefile <- 'ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE3nnn/GSE3156/matrix/GSE3156_series_matrix.txt.gz'
-download.file(remotefile, destfile="data/GSE3156_series_matrix.txt.gz")
 
-url <- "ftp://ftp.ncbi.nih.gov/pub/geo/DATA/SeriesMatrix/GSE33126/"
-filenm <- "GSE33126_series_matrix.txt.gz"
-if(!file.exists("GSE33126_series_matrix.txt.gz")) download.file(paste(url, filenm, sep=""), destfile=paste0("data/",filenm))
+remotefile <- 'ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE1nnn/GSE1729/matrix/GSE1729_series_matrix.txt.gz'
+destfile <- "data/GSE1729_series_matrix.txt.gz"
+if(!file.exists(destfile)) download.file(remotefile, destfile)
+
+remotefile <- 'ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE18nnn/GSE18088/matrix/GSE18088_series_matrix.txt.gz'
+destfile <- "data/GSE18088_series_matrix.txt.gz"
+if(!file.exists(destfile)) download.file(remotefile, destfile)
+
+url <- "ftp://ftp.ncbi.nih.gov/pub/geo/DATA/SeriesMatrix/GSE33126/GSE33126_series_matrix.txt.gz"
+filenm <- "data/GSE33126_series_matrix.txt.gz"
+if(!file.exists(filenm)) download.file(url, destfile=filenm)
