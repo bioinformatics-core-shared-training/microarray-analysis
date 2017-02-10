668895e37dd6850295cd052ecceeab00213c34bd
markdunning <mark.dunning@gmail.com>
2017-02-10 11:17:25 +0000

    update with a much smaller zip file

--- make_zip.R
+++ make_zip.R
@@ -4,5 +4,7 @@ zip("Course_Data.zip",files=c("affymetrix.Rmd",
                               "downstream-tutorial.Rmd",
                               "enrichment.Rmd",
                               "estrogen/",
-                              "data/",
+                              "data/GSE1729_series_matrix.txt.gz",
+                              "data/GSE18088_series_matrix.txt.gz",
+                              "data/GSE33126_series_matrix.txt.gz",
                               "installBiocPkgs.R"))
