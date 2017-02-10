6a77f272c641250f9861addca5e766c7e06587e7
markdunning <mark.dunning@gmail.com>
2017-02-10 10:59:31 +0000

    Add course zip file

---
+++ make_zip.R
@@ -0,0 +1,8 @@
+zip("Course_Data.zip",files=c("affymetrix.Rmd",
+                              "public-data.Rmd",
+                              "de-tutorial.Rmd",
+                              "downstream-tutorial.Rmd",
+                              "enrichment.Rmd",
+                              "estrogen/",
+                              "data/",
+                              "installBiocPkgs.R"))
